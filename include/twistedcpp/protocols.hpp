cbaaed82dc54696a0dfce21b0fa7ac43fe32be70
Stephan Dollberg <s.dollberg@tum.de>
2014-01-20 19:42:48 +0100

    initial commit

---
+++ include/twistedcpp/protocols.hpp
@@ -0,0 +1,60 @@
+#ifndef TWISTED_PROTOCOLS
+#define TWISTED_PROTOCOLS
+
+using boost::asio::ip::tcp;
+
+namespace twisted {
+
+template<typename ChildProtocol>
+class basic_protocol : public std::enable_shared_from_this<ChildProtocol> {
+public:
+    typedef boost::asio::ip::tcp::socket socket_type;
+    typedef boost::asio::steady_timer timer_type;
+    typedef boost::asio::io_service::strand strand_type;
+
+    basic_protocol() {}
+
+    void set_socket(socket_type socket) {
+        _socket.reset(new socket_type(std::move(socket)));
+        _strand = boost::in_place(boost::ref(_socket->get_io_service()));
+        _timer =  boost::in_place(boost::ref(_socket->get_io_service()));
+    }
+
+    void run_protocol() {
+        auto self = static_cast<ChildProtocol*>(this)->shared_from_this();
+        boost::asio::spawn(*_strand, [this, self] (boost::asio::yield_context yield) {
+            _yield = boost::in_place(yield);
+
+            std::vector<char> buffer(1024, '\0');
+            try {
+                for(;;) {
+                    auto bytes_read = _socket->async_read_some(
+                        boost::asio::buffer(buffer), yield);
+                    static_cast<ChildProtocol*>(this)->handle_read(
+                        buffer.begin(), std::next(buffer.begin(), bytes_read));
+                }
+            } catch (std::exception& e) {
+                std::cout << "Exception in basic_protocol" << std::endl;
+            }
+        });
+    }
+
+    template<typename Iter>
+    void write(Iter begin, Iter end) {
+        boost::asio::async_write(
+            *_socket,
+            boost::asio::buffer(&*begin, std::distance(begin, end)),
+            *_yield);
+    }
+
+private:
+    boost::optional<boost::asio::yield_context> _yield;
+    std::unique_ptr<socket_type> _socket; // unique_ptr as boost::optional has no move support
+    boost::optional<timer_type> _timer;
+    boost::optional<strand_type> _strand;
+};
+
+}
+
+
+#endif
---
+++ include/twistedcpp/reactor.hpp
@@ -0,0 +1,50 @@
+#ifndef TWISTEDCPP_REACTOR_HPP
+#define TWISTEDCPP_REACTOR_HPP
+
+#include <boost/asio/io_service.hpp>
+#include <boost/asio/ip/tcp.hpp>
+#include <boost/asio/spawn.hpp>
+#include <boost/asio/steady_timer.hpp>
+#include <boost/asio/write.hpp>
+#include <boost/container/deque.hpp>
+#include <iostream>
+#include <memory>
+#include <iterator>
+
+using boost::asio::ip::tcp;
+
+namespace twisted {
+
+
+
+template<typename Protocol, typename ...ProtocolArgs>
+void run(int port, ProtocolArgs&&... protocol_args) {
+
+    boost::asio::io_service io_service;
+
+    boost::asio::spawn(io_service,
+        [&](boost::asio::yield_context yield)
+        {
+            tcp::acceptor acceptor(io_service,
+                tcp::endpoint(tcp::v4(), port));
+
+            for (;;) {
+                boost::system::error_code ec;
+                tcp::socket socket(io_service);
+                acceptor.async_accept(socket, yield[ec]);
+                if (!ec) {
+                    auto new_client = 
+                        std::make_shared<Protocol>(std::forward<ProtocolArgs>(protocol_args)...);
+                    // lazy init to avoid clutter in protocol constructors
+                    new_client->set_socket(std::move(socket));
+                    new_client->run_protocol();
+                }
+            }
+        });
+
+    io_service.run();
+}
+
+}
+
+#endif 
