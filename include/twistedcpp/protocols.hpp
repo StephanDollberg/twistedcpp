e9ca880b2b56caecd6367d468161ee77e08552fe
Stephan Dollberg <s.dollberg@tum.de>
2014-01-21 15:33:24 +0100

    removed whitespace

--- include/twistedcpp/protocols.hpp
+++ include/twistedcpp/protocols.hpp
@@ -30,7 +30,7 @@ public:
                         boost::asio::buffer(buffer), yield);
                     checked_on_message(buffer.begin(), std::next(buffer.begin(), bytes_read));
                 }
-            } catch (boost::system::system_error& connection_error) { // network errors
+            } catch(boost::system::system_error& connection_error) { // network errors
                 print_connection_error(connection_error);
                 this_protocol().on_disconnect();
             } catch(const std::exception& excep) { // errors from user protocols
