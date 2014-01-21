8fff5c5361033314b37ee821af8be20c470a4dac
Stephan Dollberg <s.dollberg@tum.de>
2014-01-21 14:43:42 +0100

    added on_error

--- include/twistedcpp/protocols.hpp
+++ include/twistedcpp/protocols.hpp
@@ -28,12 +28,13 @@ public:
                 for(;;) {
                     auto bytes_read = _socket->async_read_some(
                         boost::asio::buffer(buffer), yield);
-                    this_protocol().on_message(
-                        buffer.begin(), std::next(buffer.begin(), bytes_read));
+                    checked_on_message(buffer.begin(), std::next(buffer.begin(), bytes_read));
                 }
             } catch (boost::system::system_error& connection_error) {
                 print_connection_error(connection_error);
                 this_protocol().on_disconnect();
+            } catch(const std::exception& excep) {
+                print_exception_what(excep);
             }
         });
     }
@@ -47,12 +48,30 @@ public:
     }
 
     void on_disconnect() {}
+    void on_error(std::exception_ptr eptr) {
+        std::rethrow_exception(eptr);
+    }
 
 private:
     void print_connection_error(const boost::system::system_error& connection_error) const {
         std::cerr << "Client disconnected with code " 
                   << connection_error.what() 
                   << std::endl;
+    }
+
+    void print_exception_what(const std::exception& excep) {
+        std::cerr << "Killing connection, exception in client handler: "
+                  << excep.what()
+                  << std::endl;
+    }
+
+    template<typename Iter>
+    void checked_on_message(Iter begin, Iter end) {
+        try {
+            this_protocol().on_message(begin, end);
+        } catch (...) {
+            this_protocol().on_error(std::current_exception());
+        }
     }
 
     /*
