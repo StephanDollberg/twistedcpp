51d466db3101eb4385bfb95e53928ebda1d8197a
Stephan Dollberg <s.dollberg@tum.de>
2014-01-22 15:10:32 +0100

    added buffer interface
    Not sure if we keep this, yet.

--- include/twistedcpp/protocols/basic_protocol.hpp
+++ include/twistedcpp/protocols/basic_protocol.hpp
@@ -9,8 +9,13 @@ public:
     typedef boost::asio::ip::tcp::socket socket_type;
     typedef boost::asio::steady_timer timer_type;
     typedef boost::asio::io_service::strand strand_type;
+    typedef std::vector<char> buffer_type;
+    typedef buffer_type::iterator buffer_iterator;
+    typedef buffer_type::const_iterator const_buffer_iterator;
 
-    basic_protocol() {}
+    basic_protocol()
+        : _read_buffer(1024),
+            _read_index(_read_buffer.begin()) {}
 
     void set_socket(socket_type socket) {
         _socket.reset(new socket_type(std::move(socket)));
@@ -23,12 +28,11 @@ public:
         boost::asio::spawn(*_strand, [this, self] (boost::asio::yield_context yield) {
             _yield = boost::in_place(yield);
 
-            std::vector<char> buffer(1024, '\0');
             try {
                 for(;_socket->is_open();) {
-                    auto bytes_read = _socket->async_read_some(
-                        boost::asio::buffer(buffer), yield);
-                    checked_on_message(buffer.begin(), std::next(buffer.begin(), bytes_read));
+                    expand_buffer();
+                    auto bytes_read = _socket->async_read_some(read_buffer(), yield);
+                    checked_on_message(_read_index, std::next(_read_index, bytes_read));
                 }
             } catch(boost::system::system_error& connection_error) { // network errors
                 print_connection_error(connection_error);
@@ -37,6 +41,10 @@ public:
                 print_exception_what(excep);
             }
         });
+    }
+
+    boost::asio::mutable_buffers_1 read_buffer() {
+        return boost::asio::buffer(&*_read_index, std::distance(_read_index, _read_buffer.end()));
     }
 
     template<typename Iter>
@@ -71,6 +79,14 @@ private:
                   << std::endl;
     }
 
+    void expand_buffer() {
+        auto range = std::distance(_read_buffer.begin(), _read_index);
+        if(range < 1024) {
+            _read_buffer.resize(_read_buffer.size() * 2);
+            _read_index = std::next(_read_buffer.begin(), range);
+        }
+    }
+
     template<typename Iter>
     void checked_on_message(Iter begin, Iter end) {
         try {
@@ -94,6 +110,10 @@ private:
         return *static_cast<ChildProtocol*>(this);
     }
 
+protected:
+    buffer_type _read_buffer;
+    buffer_iterator _read_index;
+private:
     boost::optional<boost::asio::yield_context> _yield;
     std::unique_ptr<socket_type> _socket; // unique_ptr as boost::optional has no move support
     boost::optional<timer_type> _timer;
