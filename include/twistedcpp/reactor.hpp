f4de2d05ffbb517715bd0518d36643201f829db4
Stephan Dollberg <stephan.dollberg@gmail.com>
2014-02-06 09:17:28 +0100

    factored context creation out of reactor

--- include/twistedcpp/reactor.hpp
+++ include/twistedcpp/reactor.hpp
@@ -72,23 +72,11 @@ private:
                            ssl_options ssl_ops) {
         using boost::asio::ip::tcp;
 
-        auto socket_factory = [=] {
-            boost::asio::ssl::context _context(boost::asio::ssl::context::sslv23_server);
-            _context.set_options(
-                boost::asio::ssl::context::default_workarounds |
-                boost::asio::ssl::context::no_sslv2 |
-                boost::asio::ssl::context::single_dh_use);
-            _context.set_password_callback([=](
-                std::size_t /* max_length */,
-                boost::asio::ssl::context::password_purpose /* purpose */) {
-                return ssl_ops.password();
-            });
-            _context.use_certificate_chain_file(ssl_ops.certificate());
-            _context.use_private_key_file(ssl_ops.key(),
-                                          boost::asio::ssl::context::pem);
+        auto context = make_ssl_context(ssl_ops);
 
+        auto socket_factory = [&] {
             return std::unique_ptr<detail::ssl_socket>(
-                new detail::ssl_socket(_io_service, _context));
+                new detail::ssl_socket(_io_service, context));
         };
 
         run_loop(acceptor, std::move(factory), yield, socket_factory);
--- include/twistedcpp/ssl_options.hpp
+++ include/twistedcpp/ssl_options.hpp
@@ -8,17 +8,12 @@ public:
         : _certificate(std::move(certificate)), _key(std::move(key)),
           _password(std::move(password)) {}
 
-    const std::string& certificate() const {
-        return _certificate;
-    }
+    const std::string& certificate() const { return _certificate; }
 
-    const std::string& key() const {
-        return _key;
-    }
+    const std::string& key() const { return _key; }
 
-    const std::string& password() const {
-        return _password;
-    }
+    const std::string& password() const { return _password; }
+
 private:
     std::string _certificate;
     std::string _key;
@@ -28,6 +23,24 @@ private:
 ssl_options default_ssl_options(std::string certificate, std::string key,
                                 std::string password) {
     return ssl_options(certificate, key, password);
+}
+
+boost::asio::ssl::context make_ssl_context(ssl_options ssl_ops) {
+    boost::asio::ssl::context context(
+        boost::asio::ssl::context::sslv23_server);
+    context.set_options(boost::asio::ssl::context::default_workarounds |
+                         boost::asio::ssl::context::no_sslv2 |
+                         boost::asio::ssl::context::single_dh_use);
+    context.set_password_callback([=](
+        std::size_t /* max_length */,
+        boost::asio::ssl::context::password_purpose /* purpose */) {
+        return ssl_ops.password();
+    });
+    context.use_certificate_chain_file(ssl_ops.certificate());
+    context.use_private_key_file(ssl_ops.key(),
+                                  boost::asio::ssl::context::pem);
+
+    return context;
 }
 
 } // namespace twisted
