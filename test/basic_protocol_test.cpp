03d2838bdd5d22285bd7dc87f71739e5e98d8390
Stephan Dollberg <stephan.dollberg@gmail.com>
2014-02-09 14:36:55 +0100

    added on_disconnect test

--- test/basic_protocol_test.cpp
+++ test/basic_protocol_test.cpp
@@ -98,4 +98,46 @@ TEST_CASE("on_error test", "[tcp][reactor]") {
     CHECK(buffer == input);
 }
 
-TEST_CASE("on_disconnect test", "[tcp][reactor]") {}
+struct disconnect_test_protocol
+    : twisted::basic_protocol<disconnect_test_protocol> {
+
+    disconnect_test_protocol(bool& disconnected)
+        : _disconnected(disconnected) {}
+
+    template <typename Iter>
+    void on_message(Iter /* begin */, Iter /* end */) {}
+
+    void on_disconnect() { _disconnected = true; }
+
+    bool& _disconnected;
+};
+
+TEST_CASE("on_disconnect test", "[tcp][reactor]") {
+    twisted::reactor reac;
+    bool disconnected = false;
+    auto fut = std::async(std::launch::async, [&]() {
+        reac.listen_tcp(
+            50000, [&]() { return disconnect_test_protocol(disconnected); });
+        reac.run();
+    });
+
+    boost::asio::io_service io_service;
+    tcp::socket socket(io_service);
+
+    BOOST_SCOPE_EXIT_TPL((&reac)(&socket)) {
+        reac.stop();
+        if (socket.is_open()) {
+            socket.close();
+        }
+    }
+    BOOST_SCOPE_EXIT_END
+
+    std::this_thread::sleep_for(std::chrono::milliseconds(3));
+
+    socket.connect(tcp::endpoint(
+        boost::asio::ip::address::from_string("127.0.0.1"), 50000));
+
+    socket.close();
+    std::this_thread::sleep_for(std::chrono::milliseconds(3));
+    CHECK(disconnected == true);
+}
